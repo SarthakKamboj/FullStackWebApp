7fdf4cf6c9afe804160879e60a1e1f53187e985f
SarthakKamboj <sarthakkamboj22@gmail.com>
2020-10-09 18:19:19 -0700

    re-created front-end nextjs app

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,11 @@
+// @ts-nocheck
+const Index = () => {
+    const word:string = "this was created outside the return statement";
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Index;
